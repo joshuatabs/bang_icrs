4c04f22d07d39945cb979810d60492376abe4407
joshuatabs <118796042+joshuatabs@users.noreply.github.com>
2022-11-22 14:47:33 +0800

    Add files via upload

---
+++ bang/src/main/java/com/teamcute/bang/BangApplication.java
@@ -0,0 +1,13 @@
+package com.teamcute.bang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BangApplication.class, args);
+	}
+
+}
---
+++ bang/src/test/java/com/teamcute/bang/BangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamcute.bang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
