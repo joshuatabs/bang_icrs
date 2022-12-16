f6d4f9991153cca15d9e41d5850e1576314f6a11
JOSHUA TABOSO <joshua.taboso@cit.edu>
2022-12-17 04:22:16 +0800

    Update OrderEntity

--- bang/src/main/java/com/teamcute/bang/Entity/OrderEntity.java
+++ bang/src/main/java/com/teamcute/bang/Entity/OrderEntity.java
@@ -1,15 +1,12 @@
 package com.teamcute.bang.Entity;
 
-import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
