314e4558cb4ee3cf5abf3a55c87fa735985bd856
JOSHUA TABOSO <joshua.taboso@cit.edu>
2022-12-17 04:23:44 +0800

    Update Venue COntroller, Entity

--- bang/src/main/java/com/teamcute/bang/Entity/VenueEntity.java
+++ bang/src/main/java/com/teamcute/bang/Entity/VenueEntity.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 @Entity
