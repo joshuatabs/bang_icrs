65b25b2ddfe657b5a1cb84964f11de466d42081d
JOSHUA TABOSO <joshua.taboso@cit.edu>
2022-12-18 00:27:55 +0800

    Update UserController

--- bang/src/main/java/com/teamcute/bang/Controller/UserController.java
+++ bang/src/main/java/com/teamcute/bang/Controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,22 +13,24 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.teamcute.bang.Entity.UserEntity;
 import com.teamcute.bang.Service.UserService;
 
 
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 	//implement the service class
 	@Autowired
 	UserService userv;
 	
+	@CrossOrigin
 	@PostMapping("/postUser")
-	//Create/Insert
-	public UserEntity inserUser(@RequestBody UserEntity user) {
+		//Create/Insert
+		public UserEntity inserUser(@RequestBody UserEntity user) {
 		return userv.insertUser(user); //calling the method created in the service class
 	}
 	
